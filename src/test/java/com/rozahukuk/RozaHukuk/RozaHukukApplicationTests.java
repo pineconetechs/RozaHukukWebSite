58d21e90185783c5496dccd09dcbe24044142579
ibrahim <ibrahim.tarla@triusbilisim.com>
2020-01-20 11:09:40 +0300

    ad

---
+++ src/test/java/com/rozahukuk/RozaHukuk/RozaHukukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozahukuk.RozaHukuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozaHukukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
